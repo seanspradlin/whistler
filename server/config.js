72483a6ab9e1b57f28e00b15a0b0c24dd44c25b0
Sean Spradlin <seanspradlin@gmail.com>
2017-03-30 23:23:30 -0500

    Added configs for google auth

--- server/config.js
+++ server/config.js
@@ -1,6 +1,11 @@
 module.exports = {
+  env: process.env.NODE_ENV || 'development',
   db: process.env.MONGO_URL || 'mongodb://mongo/whistler',
   logLevel: process.env.LOG_LEVEL || 'debug',
   port: process.env.PORT || 8080,
+  google: {
+    id: process.env.GOOGLE_CLIENT_ID || '',
+    secret: process.env.GOOGLE_CLIENT_SECRET || '',
+  },
 };
 
