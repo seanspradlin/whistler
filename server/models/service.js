62ceae0df2d6906f3ab6574dee45fa72ea1c7063
Sean Spradlin <seanspradlin@gmail.com>
2017-04-02 18:49:14 -0500

    Generate secrets for services

--- server/models/service.js
+++ server/models/service.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('../lib/mongoose');
 
 const Schema = mongoose.Schema;
@@ -11,10 +12,27 @@ const Service = new Schema({
     type: String,
     required: true,
   },
-  subscribers: [{
+  secret: {
+    type: String,
+    required: true,
+  },
+  project: {
     type: Schema.Types.ObjectId,
-    ref: 'User',
-  }],
+    ref: 'Project',
+    required: true,
+  },
+});
+
+Service.methods.generateSecret = function generateSecret() {
+  this.secret = crypto.randomBytes(32).toString('base64');
+  return this.secret;
+};
+
+Service.pre('save', (next) => {
+  if (!this.secret) {
+    this.generateSecret();
+  }
+  next();
 });
 
 module.exports = mongoose.model('Service', Service);
