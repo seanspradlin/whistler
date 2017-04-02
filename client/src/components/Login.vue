281886c197ed5f7e192c57c74a4754ca3745842e
Sean Spradlin <seanspradlin@gmail.com>
2017-04-01 23:35:36 -0500

    made a basic google login button

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,8 +1,21 @@
-<template>
-
+<template lang="pug">
+  .login
+    h1 Login
+    div(ref="signInButton") sign in
 </template>
 
 <script>
-
+export default {
+  name: 'login',
+  mounted() {
+    window.gapi.load('auth2', () => {
+      const auth2 = window.gapi.auth2.init();
+      auth2.attachClickHandler(this.$refs.signInButton, {}, (googleUser) => {
+        const token = googleUser.getAuthResponse().id_token;
+        this.$http.post('/api/account/google', { token });
+      });
+    });
+  },
+};
 </script>
 
