23e6c13b35eb18c14a4be0f7df16e25baf7eb0b1
Sean Spradlin <seanspradlin@gmail.com>
2017-04-01 23:32:04 -0500

    Fully implement google auth

--- server/routes/account.js
+++ server/routes/account.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const request = require('request');
+const request = require('request').defaults({ json: true });
 const Promise = require('bluebird');
 const User = require('../models').User;
 const Errors = require('../lib/errors');
@@ -9,7 +9,7 @@ const router = express.Router({ mergeParams: true });
 
 function redeemGoogleToken(token) {
   if (!token) {
-    return Promise.reject(new Errors.Generic('accessToken required'), 400);
+    return Promise.reject(new Errors.Generic('token required'), 400);
   }
 
   return new Promise((resolve, reject) => {
@@ -35,6 +35,29 @@ function redeemGoogleToken(token) {
  *
  * @apiPermission user
  */
+router.get('/', (req, res, next) => {
+  if (!req.session.user) {
+    next(new Errors.Unauthorized());
+  } else {
+    res.body = req.session.user;
+    next();
+  }
+});
+
+/**
+ * @api {post} /account/logout Logout
+ * @apiName PostAccountLogout
+ * @apiGroup Account
+ * @apiVersion 0.1.0
+ * @apiDescription Logout of your account.
+ *
+ * @apiPermission public
+ */
+router.post('/logout', (req, res, next) => {
+  delete req.session.user;
+  res.status(204);
+  next();
+});
 
 /**
  * @api {post} /account/google Login with Google
@@ -43,35 +66,39 @@ function redeemGoogleToken(token) {
  * @apiVersion 0.1.0
  * @apiDescription Use Google to log in and then send their access token to generate a user session
  *
- * @apiParam {String} accessToken Google account access token
+ * @apiParam {String} token Google account access token
  *
- * @apiSuccess  {ObjectId}  _id       Unique user identifier
- * @apiSuccess  {String}    name      Name
- * @apiSuccess  {String}    email     Email
+ * @apiSuccess  {String}  _id       Unique user identifier
+ * @apiSuccess  {String}  name      Name
+ * @apiSuccess  {String}  email     Email
  *
  * @apiUse UnauthorizedError
+ *
+ * @apiPermission user
  */
 router.post('/google', (req, res, next) => {
-  redeemGoogleToken(req.body.accessToken)
+  redeemGoogleToken(req.body.token)
     .then((result) => {
       if (result.aud !== config.google.id) {
         return Promise.reject(new Errors.Unauthorized());
       }
       return User.findOne({ googleId: result.sub })
         .then((user) => {
-          if (!user) {
-            const newUser = new User({
-              name: result.name,
-              email: result.email,
-              googleId: result.sub,
-            });
-            return newUser.save();
+          if (user) {
+            return Promise.resolve(user);
           }
-          return Promise.resolve(user);
+
+          const newUser = new User({
+            name: result.name,
+            email: result.email,
+            googleId: result.sub,
+            picture: result.picture,
+          });
+          return newUser.save();
         });
     })
-    .then(user => req.login(user))
     .then((user) => {
+      req.session.user = user;
       res.body = user;
       next();
     })
