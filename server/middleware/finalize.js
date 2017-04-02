0955b4e2aebbaddb2ac4ae5bd743b1fa7a9c2309
Sean Spradlin <seanspradlin@gmail.com>
2017-04-01 23:31:12 -0500

    Correctly return error message

--- server/middleware/finalize.js
+++ server/middleware/finalize.js
@@ -4,7 +4,7 @@ module.exports = () =>
   (req, res) => {
     if (!res.body && res.statusCode !== 204) {
       res.statusCode = 404;
-      res.message = { error: http.STATUS_CODES['404'] };
+      res.body = { error: http.STATUS_CODES['404'] };
     }
     res.json(res.body);
   };
