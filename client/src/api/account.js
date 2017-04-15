1119404e0e6bba1f30ada8ff2ce9ef7af42ed612
Sean Spradlin <seanspradlin@gmail.com>
2017-04-15 07:23:10 -0500

    added method stubs

--- client/src/api/account.js
+++ client/src/api/account.js
@@ -1,4 +1,14 @@
 export default {
+  getAccount() {
+    return Promise.reject(new Error('not implemented'));
+  },
 
+  login() {
+    return Promise.reject(new Error('not implemented'));
+  },
+
+  logout() {
+    return Promise.reject(new Error('not implemented'));
+  },
 };
 
--- client/src/api/projects.js
+++ client/src/api/projects.js
@@ -1,4 +1,22 @@
 export default {
+  getProjects(filter) {
+    return Promise.reject(new Error('not implemented'));
+  },
 
+  getProjectById(id) {
+    return Promise.reject(new Error('not implemented'));
+  },
+
+  createProject({ name }) {
+    return Promise.reject(new Error('not implemented'));
+  },
+
+  updateProject(id, { name }) {
+    return Promise.reject(new Error('not implemented'));
+  },
+
+  deleteProject(id) {
+    return Promise.reject(new Error('not implemented'));
+  },
 };
 
