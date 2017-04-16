41150446122bd8c6b9ef85e38a898861df3d8ef6
Sean Spradlin <seanspradlin@gmail.com>
2017-04-16 09:16:47 -0500

    more stubs

--- client/src/api/services.js
+++ client/src/api/services.js
@@ -1,4 +1,22 @@
 export default {
+  getServices(filter) {
+    return Promise.reject(new Error('not implemented'));
+  },
 
+  getServiceById(id) {
+    return Promise.reject(new Error('not implemented'));
+  },
+
+  createService({ name, env, project }) {
+    return Promise.reject(new Error('not implemented'));
+  },
+
+  updateService(id, { name, env, project }) {
+    return Promise.reject(new Error('not implemented'));
+  },
+
+  deleteProject(id) {
+    return Promise.reject(new Error('not implemented'));
+  },
 };
 
--- client/src/api/tickets.js
+++ client/src/api/tickets.js
@@ -1,4 +1,30 @@
 export default {
+  getTickets(filter) {
+    return Promise.reject(new Error('not implemented'));
+  },
 
+  getTicketById(id) {
+    return Promise.reject(new Error('not implemented'));
+  },
+
+  closeTicket(id) {
+    return Promise.reject(new Error('not implemented'));
+  },
+
+  addComment(id, message) {
+    return Promise.reject(new Error('not implemented'));
+  },
+
+  getAllComments(id) {
+    return Promise.reject(new Error('not implemented'));
+  },
+
+  editComment(ticketId, commentId, message) {
+    return Promise.reject(new Error('not implemented'));
+  },
+
+  deleteComment(ticketId, commentId) {
+    return Promise.reject(new Error('not implemented'));
+  },
 };
 
