c4e25e240a3404f20f420e516b47af3ab07e4266
Sean Spradlin <seanspradlin@gmail.com>
2017-04-18 11:26:22 -0500

    Created basic implementation of user fetch

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,9 +9,41 @@ const router = express.Router({ mergeParams: true });
  * @apiName GetUsers
  * @apiGroup Users
  * @apiVersion 0.1.0
+ *
+ * @apiParam  {String}  name
+ * @apiParam  {String}  email
+ *
+ * @apiSuccess  {Object[]}  users
+ * @apiSuccess  {String}    users.name
+ * @apiSuccess  {String}    users.email
+ * @apiSuccess  {String}    users.googleId
+ * @apiSuccess  {String}    users.picture
+ *
+ * @apiUse UnauthorizedError
+ *
+ * @apiPermission user
  */
 router.get('/', (req, res, next) => {
-  next(new Errors.Generic('Not implemented'));
+  if (!req.session.user) {
+    next(new Errors.Unauthorized());
+  } else {
+    const options = {};
+
+    if (req.body.email) {
+      options.email = req.body.email;
+    }
+
+    if (req.body.name) {
+      options.name = req.body.name;
+    }
+
+    User.find(options)
+      .then((users) => {
+        res.body = users;
+        next();
+      })
+      .catch(next);
+  }
 });
 
 /**
@@ -19,9 +51,29 @@ router.get('/', (req, res, next) => {
  * @apiName GetUsersId
  * @apiGroup Users
  * @apiVersion 0.1.0
+ *
+ * @apiParam  {String}  userId
+ *
+ * @apiSuccess  {String}  name
+ * @apiSuccess  {String}  email
+ * @apiSuccess  {String}  googleId
+ * @apiSuccess  {String}  picture
+ *
+ * @apiUse UnauthorizedError
+ *
+ * @apiPermission user
  */
 router.get('/:userId', (req, res, next) => {
-  next(new Errors.Generic('Not implemented'));
+  if (!req.session.user) {
+    next(new Errors.Unauthorized());
+  } else {
+    User.findById(req.params.userId)
+      .then((user) => {
+        res.body = user;
+        next();
+      })
+      .catch(next);
+  }
 });
 
 module.exports = router;
