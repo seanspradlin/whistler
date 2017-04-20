30625a07b8a33b5bf2cd48c2e5365cf7aebce2d4
Sean Spradlin <seanspradlin@gmail.com>
2017-04-19 21:37:47 -0500

    added account store stubs

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import account from './modules/account';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({});
+export default new Vuex.Store({
+  modules: {
+    account,
+  },
+});
 
---
+++ client/src/store/modules/account.js
@@ -0,0 +1,20 @@
+const state = {
+  loggedIn: false,
+  name: null,
+  picture: null,
+  id: null,
+};
+
+const getters = {};
+
+const actions = {};
+
+const mutations = {};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
+
