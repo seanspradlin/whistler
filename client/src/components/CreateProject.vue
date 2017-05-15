ada702064e06aec124794bdec6b7552be7351616
Sean Spradlin <seanspradlin@gmail.com>
2017-05-14 23:07:46 -0500

    Got validator to work

--- client/src/components/CreateProject.vue
+++ client/src/components/CreateProject.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   .container
     form(v-on:submit.prevent="save")
-      input(v-model="name" v-validate="'required'" placeholder="Project name...")
-      span(v-show="errors.has('required')") {{ errors.first('required') }}
+      input(v-model="name" v-validate:name="'required|alpha_spaces'" type="text" name="name" placeholder="Project name...")
+      span(v-show="errors.has('name')") Name is required
       input(type="submit" value="Save")
     router-link(to="/projects") Back to projects
 </template>
@@ -10,16 +10,17 @@
 <script>
 export default {
   name: 'projects',
-  data() {
-    return {
-      name: '',
-    };
-  },
+  data: () => ({
+    name: '',
+  }),
   methods: {
     save() {
-      const project = { name: this.name };
-      this.$store.dispatch('createProject', project);
-      this.$router.push({ name: 'Projects' });
+      this.$validator.validateAll()
+        .then(() => {
+          const project = { name: this.name };
+          this.$store.dispatch('createProject', project);
+          this.$router.push({ name: 'Projects' });
+        });
     },
   },
 };
